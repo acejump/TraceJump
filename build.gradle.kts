8692aba904527d5bc5aa64ff777a94d83530188f
breandan <bre@ndan.co>
2019-11-17 17:42:52 -0500

    plugin-ify

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,59 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.3.50"
-  id("org.openjfx.javafxplugin") version "0.0.8"
-  application
+    application
+    idea apply true
+    kotlin("jvm") version "1.3.50"
+    id("org.jetbrains.intellij") version "0.4.10"
+    id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "org.tracejump"
+version = "0.1-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+        kotlinOptions.freeCompilerArgs += "-progressive"
+    }
+
+    named<Zip>("buildPlugin") {
+        dependsOn("test")
+        archiveFileName.set("TraceJump.zip")
+    }
 }
 
 dependencies {
-  implementation(kotlin("stdlib-jdk8"))
-  compile("org.bytedeco:javacv-platform:1.5.2")
-  compile("org.bytedeco:tesseract-platform:1.5.2")
-  compile("org.bytedeco:leptonica-platform:1.5.2")
-  implementation("com.1stleg:jnativehook:2.1.0")
-  implementation("org.openjfx:javafx-swing:11")
-  implementation("org.openjfx:javafx:11")
-  implementation("com.google.guava:guava:28.1-jre")
-  implementation("org.apache.lucene:lucene-core:8.2.0")
-  implementation("org.apache.lucene:lucene-queryparser:8.2.0")
+    implementation(kotlin("stdlib-jdk8"))
+    compile("org.bytedeco:javacv-platform:1.5.2")
+    compile("org.bytedeco:tesseract-platform:1.5.2")
+    compile("org.bytedeco:leptonica-platform:1.5.2")
+    implementation("com.1stleg:jnativehook:2.1.0")
+    implementation("org.openjfx:javafx-swing:11")
+    implementation("org.openjfx:javafx:11")
+    implementation("com.google.guava:guava:28.1-jre")
+    implementation("org.apache.lucene:lucene-core:8.2.0")
+    implementation("org.apache.lucene:lucene-queryparser:8.2.0")
+}
+
+intellij {
+    pluginName = "AceJump"
+    updateSinceUntilBuild = false
+    setPlugins("java")
 }
 
 tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 javafx {
-  modules("javafx.controls")
+    modules("javafx.controls")
 }
 
 configure<ApplicationPluginConvention> {
-  mainClassName = "MainKt"
+    mainClassName = "MainKt"
 }
