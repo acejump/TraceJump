ee3e832a2075a052e9c47c02ac9962595fb5e4fa
breandan <bre@ndan.co>
2020-11-30 11:05:52 -0500

    bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.4.10"
-    id("org.beryx.jlink") version "2.22.2"
+    kotlin("jvm") version "1.4.20"
+    id("org.beryx.jlink") version "2.22.3"
     id("org.openjfx.javafxplugin") version "0.0.9"
-    id("com.gluonhq.client-gradle-plugin") version "0.1.33"
-    id("com.github.ben-manes.versions") version "0.33.0"
+    id("com.gluonhq.client-gradle-plugin") version "0.1.35"
+    id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 //val javaLangVersion = JavaLanguageVersion.of(11)
@@ -39,8 +39,8 @@ version = "0.2-SNAPSHOT"
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.bytedeco:javacv-platform:1.5.4")
-    implementation("org.bytedeco:tesseract-platform:1.5.4")
-    implementation("org.bytedeco:leptonica-platform:1.5.4")
+    implementation("org.bytedeco:tesseract-platform:4.1.1-1.5.4")
+    implementation("org.bytedeco:leptonica-platform:1.80.0-1.5.4")
     implementation("org.openjfx:javafx:11.0.2")
     implementation("com.google.guava:guava:30.0-jre")
     implementation("org.openjfx:javafx-swing:11.0.2")
